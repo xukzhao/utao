fc75a116331d69f3c1b36eb9cee5a6f1d8da2fa0
vonchange <vonchange@163.com>
2025-03-17 08:41:43 +0800

    [new]bug闪退修复 logutil

--- android/x5/app/src/main/java/tv/utao/x5/util/LogUtil.java
+++ android/x5/app/src/main/java/tv/utao/x5/util/LogUtil.java
@@ -8,42 +8,42 @@ import android.util.Log;
 public class LogUtil {
     private static final String DEFAULT_TAG = "UtaoApp";
     private static final String NULL_MESSAGE = "null";
-    
+
     /**
      * 记录调试信息
      */
     public static void d(String tag, String message) {
         Log.d(tag != null ? tag : DEFAULT_TAG, message != null ? message : NULL_MESSAGE);
     }
-    
+
     /**
      * 记录信息
      */
     public static void i(String tag, String message) {
         Log.i(tag != null ? tag : DEFAULT_TAG, message != null ? message : NULL_MESSAGE);
     }
-    
+
     /**
      * 记录警告
      */
     public static void w(String tag, String message) {
         Log.w(tag != null ? tag : DEFAULT_TAG, message != null ? message : NULL_MESSAGE);
     }
-    
+
     /**
      * 记录错误
      */
     public static void e(String tag, String message) {
         Log.e(tag != null ? tag : DEFAULT_TAG, message != null ? message : NULL_MESSAGE);
     }
-    
+
     /**
      * 记录错误和异常
      */
     public static void e(String tag, String message, Throwable throwable) {
         String safeTag = tag != null ? tag : DEFAULT_TAG;
         String safeMessage = message != null ? message : NULL_MESSAGE;
-        
+
         if (throwable != null) {
             String throwableMessage = throwable.getMessage();
             if (throwableMessage != null) {
@@ -55,42 +55,42 @@ public class LogUtil {
             Log.e(safeTag, safeMessage);
         }
     }
-    
+
     /**
      * 使用默认标签记录调试信息
      */
     public static void d(String message) {
         d(DEFAULT_TAG, message);
     }
-    
+
     /**
      * 使用默认标签记录信息
      */
     public static void i(String message) {
         i(DEFAULT_TAG, message);
     }
-    
+
     /**
      * 使用默认标签记录警告
      */
     public static void w(String message) {
         w(DEFAULT_TAG, message);
     }
-    
+
     /**
      * 使用默认标签记录错误
      */
     public static void e(String message) {
         e(DEFAULT_TAG, message);
     }
-    
+
     /**
      * 使用默认标签记录错误和异常
      */
     public static void e(String message, Throwable throwable) {
         e(DEFAULT_TAG, message, throwable);
     }
-    
+
     /**
      * 记录异常
      */
@@ -101,11 +101,11 @@ public class LogUtil {
             e(tag, "Unknown exception");
         }
     }
-    
+
     /**
      * 使用默认标签记录异常
      */
     public static void exception(Throwable throwable) {
         exception(DEFAULT_TAG, throwable);
     }
-} 
+}
